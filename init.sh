971172e432913d6a65eb5d830b3744f2e59e5e32
YS Wong <ys.wong@g-asiapac.com>
2024-06-10 15:35:41 +0800

    updated region input

--- init.sh
+++ init.sh
@@ -83,9 +83,10 @@ create_iam_role() {
     done
 
     CUSTOMER_NAME=$CUSTOMER_NAME_INPUT
+    CUSTOMER_NAME_FOR_DESCRIPTION=$(echo "$CUSTOMER_NAME_INPUT" | tr '-' '\040' | tr _ ' ')
     ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
     curl --silent -o $METADATA_FILE $METADATA_URL
-    aws iam create-saml-provider --saml-metadata-document file://$METADATA_FILE --name $PROVIDER_NAME --query 'SAMLProviderArn'
+    IDP_ARN=$(aws iam create-saml-provider --saml-metadata-document file://$METADATA_FILE --name $PROVIDER_NAME --query 'SAMLProviderArn')
     Tech_ROLE_NAME=$CUSTOMER_NAME"-SSO-Tech"
     Billing_ROLE_NAME=$CUSTOMER_NAME"-SSO-Billing"
     TRUST_RELATIONSHIP_FILE="trust-relationship.json"
@@ -124,8 +125,8 @@ EOL
   ]
 }
 EOL
-    aws iam create-role --role-name $Tech_ROLE_NAME --assume-role-policy-document file://$TRUST_RELATIONSHIP_FILE --query 'Role.Arn' --max-session-duration 43200
-    aws iam create-role --role-name $Billing_ROLE_NAME --assume-role-policy-document file://$TRUST_RELATIONSHIP_FILE --query 'Role.Arn'
+    TECH_ROLE_ARN=$(aws iam create-role --role-name $Tech_ROLE_NAME --assume-role-policy-document file://$TRUST_RELATIONSHIP_FILE --query 'Role.Arn' --max-session-duration 43200)
+    BILLING_ROLE_ARN=$(aws iam create-role --role-name $Billing_ROLE_NAME --assume-role-policy-document file://$TRUST_RELATIONSHIP_FILE --query 'Role.Arn')
     aws iam put-role-policy --role-name $Billing_ROLE_NAME --policy-name CostExplorerPolicy --policy-document file://$COST_EXPLORER_FILE
     aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --role-name $Tech_ROLE_NAME
     aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/job-function/Billing --role-name $Tech_ROLE_NAME
@@ -135,13 +136,35 @@ EOL
     rm $TRUST_RELATIONSHIP_FILE $COST_EXPLORER_FILE $METADATA_FILE
 }
 
+check_region() {
+    echo "[0] Malaysia"
+    echo "[1] Singapore"
+    echo "[2] Indonesia"
+    echo "[3] Vietnam"
+    read -p "Enter your choice (0-3): " check_region
+
+    case $check_region in
+        0) ACCOUNT_REGION="Malaysia";;
+        1) ACCOUNT_REGION="Singapore";;
+        2) ACCOUNT_REGION="Indonesia";;
+        3) ACCOUNT_REGION="Vietnam";;
+        *) echo "Invalid input. Try again!"
+    esac
+
+    echo 'Below are the roles for Keycloak realm roles registration:'
+    echo ${TECH_ROLE_ARN#\"}','${IDP_ARN#\"}
+    echo 'Technical Role for AWS PMA Account '$CUSTOMER_NAME_FOR_DESCRIPTION '('$CUSTOMER_NAME_FOR_DESCRIPTION' '$ACCOUNT_REGION')'
+    echo ${BILLING_ROLE_ARN#\"}','${IDP_ARN#\"}
+    echo 'Billing Role for AWS PMA Account '$CUSTOMER_NAME_FOR_DESCRIPTION '('$CUSTOMER_NAME_FOR_DESCRIPTION' '$ACCOUNT_REGION')'
+}
+
 check_type_account() {
     echo "[0] AWS Root Account (RA)"
     echo "[1] AWS PMA Account"
-    read choose_type_account
+    read -p "Enter your account type (0 & 1): " choose_type_account
     case $choose_type_account in
-        0) create_ra; create_iam_role; delete_default_vpc ;;
-        1) create_ra; create_iam_role ;;
+        0) create_ra; create_iam_role; check_region; delete_default_vpc ;;
+        1) create_ra; create_iam_role; check_region ;;
         *) echo 'Sorry, try again' >&2 ;;
     esac
 }
